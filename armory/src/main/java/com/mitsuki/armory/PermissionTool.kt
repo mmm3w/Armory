1834846da5860280f0c45772f978a3d85aba0355
wangzhichen <877062007@qq.com>
2021-08-25 18:39:18 +0800

    update)

--- armory/src/main/java/com/mitsuki/armory/PermissionTool.kt
+++ armory/src/main/java/com/mitsuki/armory/PermissionTool.kt
@@ -2,29 +2,68 @@ package com.mitsuki.armory
 
 import android.content.Context
 import android.content.pm.PackageManager
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 
-object PermissionTool {
 
+class PermissionTool(private val activityProvider: () -> ComponentActivity) {
 
-    fun checkSelfPermission(context: Context, permission: String): Boolean {
-        return ContextCompat.checkSelfPermission(
-            context,
-            permission
-        ) == PackageManager.PERMISSION_GRANTED
+    companion object {
+        fun checkSelfPermission(context: Context, permission: String): Boolean {
+            return ContextCompat.checkSelfPermission(
+                context,
+                permission
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+        fun checkSelfPermission(context: Context, permission: Array<String>): Boolean {
+            for (item in permission) {
+                if (ContextCompat.checkSelfPermission(context, item)
+                    != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return false
+                }
+            }
+            return true
+        }
     }
 
+    private val permissionLauncher =
+        activityProvider().registerForActivityResult(ActivityResultContracts.RequestPermission()) {
 
-    fun checkSelfPermission(context: Context, permission: Array<String>): Boolean {
-        for (item in permission) {
-            if (ContextCompat.checkSelfPermission(context, item)
-                != PackageManager.PERMISSION_GRANTED
-            ) {
-                return false
+        }
+
+    private val multiplePermissionLauncher =
+        activityProvider().registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+
+        }
+
+    fun requestPermission(code: Int, permission: String, result: ((Boolean) -> Unit)? = null) {
+        activityProvider.invoke().apply {
+            if (checkSelfPermission(this, permission)) {
+                result?.invoke(true)
+            } else {
+                //
+                permissionLauncher.launch(permission)
             }
         }
-        return true
     }
 
+    fun requestPermission(
+        code: Int,
+        permission: Array<String>,
+        result: ((List<String>, List<String>) -> Unit)? = null
 
+    ) {
+        activityProvider.invoke().apply {
+            if (checkSelfPermission(this, permission)) {
+                result?.invoke(permission.toList(), emptyList())
+            } else {
+                multiplePermissionLauncher.launch(permission)
+            }
+        }
+    }
 }
+
+fun ComponentActivity.permissionTool() :PermissionTool = PermissionTool { this }
