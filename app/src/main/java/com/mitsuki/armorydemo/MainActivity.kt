b33695be217ec1e84de3993671c67574bd3ea542
Mitsuki <psnhewzc@gmail.com>
2021-10-07 17:00:09 +0800

    update)

--- app/src/main/java/com/mitsuki/armorydemo/MainActivity.kt
+++ app/src/main/java/com/mitsuki/armorydemo/MainActivity.kt
@@ -3,11 +3,24 @@ package com.mitsuki.armorydemo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mitsuki.armorydemo.databinding.ActivityMainBinding
+import com.mitsuki.armory.base.permission.readStorePermissionLauncher
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
+    private val readStorePermissionLauncher = readStorePermissionLauncher()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        readStorePermissionLauncher.launch {  }
     }
 }
