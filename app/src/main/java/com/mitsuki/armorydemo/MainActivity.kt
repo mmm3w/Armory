46bdf782857cf858f48cfb7628165130f2053db9
wangzhichen <26341548+mmm3w@users.noreply.github.com>
2021-11-17 18:10:26 +0800

    输入法高度测量

--- app/src/main/java/com/mitsuki/armorydemo/MainActivity.kt
+++ app/src/main/java/com/mitsuki/armorydemo/MainActivity.kt
@@ -2,25 +2,26 @@ package com.mitsuki.armorydemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import com.mitsuki.armory.inputmeasure.InputHeight
 import com.mitsuki.armorydemo.databinding.ActivityMainBinding
-import com.mitsuki.armory.base.permission.readStorePermissionLauncher
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val readStorePermissionLauncher = readStorePermissionLauncher()
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(root) }
-
-
+        InputHeight.callback {
+            ViewCompat.animate(binding.mainInput).also { viewAnimator ->
+                viewAnimator.cancel()
+                viewAnimator.translationY((-it).toFloat())
+                    .setDuration(150)
+                    .start()
+            }
+        }
+        InputHeight.bindMeasure(this)
     }
 
-    override fun onResume() {
-        super.onResume()
-        readStorePermissionLauncher.launch {  }
-    }
 }
--- app/src/main/java/com/mitsuki/armorydemo/MyApplication.kt
+++ app/src/main/java/com/mitsuki/armorydemo/MyApplication.kt
@@ -1,6 +1,11 @@
 package com.mitsuki.armorydemo
 
 import android.app.Application
+import com.mitsuki.armory.inputmeasure.InputHeight
 
 class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        InputHeight.init(this)
+    }
 }
