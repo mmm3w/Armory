02ca16fe2463667b7e24bfa51aa98c21694d1f1d
Mitsuki <26341548+mmm3w@users.noreply.github.com>
2024-08-30 10:58:00 +0800

    jitpack yml

--- imagegesture/build.gradle.kts
+++ imagegesture/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -30,14 +31,27 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    resourcePrefix("m_armory_image_gesture")
 }
 
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.mitsuki.armory"
+            artifactId = "imagegesture"
+            version = "1.0.0"
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
